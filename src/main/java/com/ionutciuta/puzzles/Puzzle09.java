ae9e3369d3ec1fa712d76183ae735b4826c4f91b
Ionuț Ciuta <ionutciuta24@gmail.com>
2022-12-09 18:41:09 +0200

    Puzzle 09 - Setup

---
+++ src/main/java/com/ionutciuta/puzzles/Puzzle09.java
@@ -0,0 +1,32 @@
+package com.ionutciuta.puzzles;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class Puzzle09 extends Puzzle<Integer> {
+
+    @Override
+    public Integer solvePart1(String inputFile) {
+        var file = new File(inputFile);
+        var result = 0;
+        try (var it = FileUtils.lineIterator(file)) {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected Integer solvePart2(String inputFile) {
+        var file = new File(inputFile);
+        var result = 0;
+        try (var it = FileUtils.lineIterator(file)) {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/ionutciuta/puzzles/Puzzle09Test.java
@@ -0,0 +1,39 @@
+package com.ionutciuta.puzzles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Puzzle09Test implements PuzzleTest {
+    private final Puzzle09 puzzle = new Puzzle09();
+
+    @Test
+    @Override
+    public void shouldReturnExpectedTestResult_part1() {
+        final var result = puzzle.solvePart1ForTestInput();
+        assertEquals(0, result);
+    }
+
+    @Test
+    @Override
+    public void shouldReturnResult_part1() {
+        final var result = puzzle.solvePart1();
+        System.out.println(result);
+        assertEquals(0, result);
+    }
+
+    @Test
+    @Override
+    public void shouldReturnExpectedTestResult_part2() {
+        final var result = puzzle.solvePart2ForTestInput();
+        assertEquals(0, result);
+    }
+
+    @Test
+    @Override
+    public void shouldReturnResult_part2() {
+        final var result = puzzle.solvePart2();
+        System.out.println(result);
+        assertEquals(0, result);
+    }
+}
